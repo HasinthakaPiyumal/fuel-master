830a963b9ed6c9614419c212125ed659dfea3def
HasinthakaPiyumal <hasinthakapiyumal@gmail.com>
2025-01-16 09:06:21 +0530

    intialize project

---
+++ fuel_master_backend/src/main/java/com/uokse/fuelmaster/FuelMasterApplication.java
@@ -0,0 +1,13 @@
+package com.uokse.fuelmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelMasterApplication.class, args);
+	}
+
+}
