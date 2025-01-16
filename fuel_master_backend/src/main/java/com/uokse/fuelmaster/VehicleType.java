21f4a3a6496f8f39dec4337c4a460098538edb7b
KavinduAmalka <146797841+KavinduAmalka@users.noreply.github.com>
2025-01-16 18:36:20 +0530

    Add VehicleType entity

---
+++ fuel_master_backend/src/main/java/com/uokse/fuelmaster/VehicleType.java
@@ -0,0 +1,42 @@
+package com.uokse.fuelmaster;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class VehicleType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String vehicleType;
+
+    @Column(nullable = false)
+    private Long fuelQuantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public Long getFuelQuantity() {
+        return fuelQuantity;
+    }
+
+    public void setFuelQuantity(Long fuelQuantity) {
+        this.fuelQuantity = fuelQuantity;
+    }
+}
