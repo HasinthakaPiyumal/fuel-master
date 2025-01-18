142086e178731e39b6f186309aff50374f3e7cda
HasinthakaPiyumal <hasinthakapiyumal@gmail.com>
2025-01-18 13:52:34 +0530

    User repo error fixed

--- fuel_master_backend/src/main/java/com/uokse/fuelmaster/Repo/UserRepo.java
+++ fuel_master_backend/src/main/java/com/uokse/fuelmaster/Repo/UserRepo.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 @EnableJpaRepositories
 @Repository
 public interface UserRepo extends JpaRepository<User,Long> {
- Optional<User> findOneByPhoneAndPassword(Long phone, String password);
  User findByPhone(String phone);
  Optional<User> findOneByPhoneAndPassword(String phone, String password);
 
