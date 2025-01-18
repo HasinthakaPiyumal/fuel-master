929f875707131d87dd8fdd31703efe1e22b157d6
KavinduAmalka <146797841+KavinduAmalka@users.noreply.github.com>
2025-01-18 10:14:00 +0530

    Add login function

--- fuel_master_backend/src/main/java/com/uokse/fuelmaster/Service/impl/UserIMPL.java
+++ fuel_master_backend/src/main/java/com/uokse/fuelmaster/Service/impl/UserIMPL.java
@@ -1,11 +1,15 @@
 package com.uokse.fuelmaster.Service.impl;
 
+import com.uokse.fuelmaster.DTO.LoginDTO;
 import com.uokse.fuelmaster.DTO.UserDTO;
 import com.uokse.fuelmaster.Repo.UserRepo;
+import com.uokse.fuelmaster.Response.LoginResponse;
 import com.uokse.fuelmaster.Service.UserService;
 import com.uokse.fuelmaster.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserIMPL implements UserService {
@@ -26,5 +30,28 @@ public class UserIMPL implements UserService {
         userRepo.save(user);
 
         return user.getFirstName();
+    }
+
+    @Override
+    public LoginResponse loginUser(LoginDTO loginDTO) {
+      String msg="";
+      User user = userRepo.findByPhone(loginDTO.getPhone());
+      if(user!=null){
+          String inputPassword = loginDTO.getPassword();
+          String storedPassword= user.getPassword();
+          if(inputPassword.equals(storedPassword)){
+              Optional<User> validUser = userRepo.findOneByPhoneAndPassword(loginDTO.getPhone(),storedPassword);
+              if(validUser.isPresent()){
+                  return new LoginResponse("Login Success",true);
+              }else{
+                  return new LoginResponse("Login Failed",false);
+              }
+          } else{
+              return new LoginResponse("Password Not Match",false);
+          }
+
+      }else{
+          return new LoginResponse("Phone Number Not exits",false);
+      }
     }
 }
