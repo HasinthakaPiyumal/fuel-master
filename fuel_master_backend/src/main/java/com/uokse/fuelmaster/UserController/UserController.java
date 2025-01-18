adb55f22a38a63aa114023fe82355173ce7547b5
KavinduAmalka <146797841+KavinduAmalka@users.noreply.github.com>
2025-01-18 10:14:00 +0530

    Add login function

--- fuel_master_backend/src/main/java/com/uokse/fuelmaster/UserController/UserController.java
+++ fuel_master_backend/src/main/java/com/uokse/fuelmaster/UserController/UserController.java
@@ -1,10 +1,13 @@
 package com.uokse.fuelmaster.UserController;
 
 
+import com.uokse.fuelmaster.DTO.LoginDTO;
 import com.uokse.fuelmaster.DTO.UserDTO;
+import com.uokse.fuelmaster.Response.LoginResponse;
 import com.uokse.fuelmaster.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
@@ -20,6 +23,12 @@ public class UserController {
     public String saveUser(@RequestBody UserDTO userDTO ){
         String id = userService.addUser(userDTO);
         return ("User saved with ID: " + id);
+    }
+
+    @PostMapping(path="/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginDTO loginDTO){
+        LoginResponse loginResponse = userService.loginUser(loginDTO);
+        return ResponseEntity.ok(loginResponse);
     }
 
 }
