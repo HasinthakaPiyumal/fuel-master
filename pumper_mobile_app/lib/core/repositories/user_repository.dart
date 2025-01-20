2394236c31334825e4938b9603f9f342003013bf
HasinthakaPiyumal <hasinthakapiyumal@gmail.com>
2025-01-21 00:14:47 +0530

    Implement login functionality and UI updates for the Fuel Master app
    - Added new login screen and controller for user authentication.
    - Integrated GetX for state management and navigation.
    - Updated pubspec.yaml to include new dependencies.
    - Refactored main.dart to initialize services and set up the app structure.
    - Created shared components like AppDialog, AppTextField, and PrimaryButton for consistent UI.
    - Introduced HomeScreen with summary cards and transaction list.
    - Updated color scheme and assets for improved visual design.
    - Removed unused .vscode settings file and added .gitignore entry for pubspec.yaml.
    - Added new assets including app logo and various UI components.

--- pumper_mobile_app/lib/core/repositories/user_repository.dart
+++ pumper_mobile_app/lib/core/repositories/user_repository.dart
@@ -2,13 +2,37 @@ import 'package:pumper_mobile_app/core/models/user.dart';
 import 'package:pumper_mobile_app/core/services/api_service.dart';
 
 class UserRepository {
-    final ApiService _apiService = ApiService();
-    Future<User> getUser(String token) async {
-        try {
-            final response = await _apiService.get('/authenticate/user');
-            return User.fromMap(response);
-        } catch (e) {
-            rethrow;
-        }
+  final ApiService _apiService = ApiService();
+  Future<User?> getUser(String token) async {
+    try {
+      final response = await _apiService.get('/users/authenticate');
+      return User.fromMap(response);
+    } catch (e) {
+      return null;
     }
+  }
+
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      // final response = await _apiService.post('/auth/login', {
+      //     'email': email,
+      //     'password': password,
+      // });
+      print('Signing in with email and password');
+      await Future.delayed(Duration(seconds: 2));
+      Map<String, dynamic> response = {
+        'id': "1",
+        'name': 'John Doe',
+        'email': 'jogn@pumper.test',
+        'photoUrl': '',
+      };
+
+      print('Signing in with email and password');
+      return User.fromMap(response);
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
 }
