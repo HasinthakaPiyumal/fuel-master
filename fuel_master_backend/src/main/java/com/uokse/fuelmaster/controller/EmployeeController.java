0608f2a31d9ed88defb60c5db78e00fb039d9a6b
KavinduAmalka <146797841+KavinduAmalka@users.noreply.github.com>
2025-01-24 19:13:00 +0530

    Add view all employee and get special employee details using phone number functions

--- fuel_master_backend/src/main/java/com/uokse/fuelmaster/controller/EmployeeController.java
+++ fuel_master_backend/src/main/java/com/uokse/fuelmaster/controller/EmployeeController.java
@@ -1,9 +1,13 @@
 package com.uokse.fuelmaster.controller;
 
 
+import com.uokse.fuelmaster.dto.EmployeeViewDetailsDTO;
 import com.uokse.fuelmaster.service.EmployeeService;
 import com.uokse.fuelmaster.dto.Request.EmployeeDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -16,6 +20,26 @@ public class EmployeeController {
     public String saveEmployee(@RequestBody EmployeeDTO employeeDTO ){
         String id = employeeService.addEmployee(employeeDTO);
         return ("User saved with ID : " + id);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllEmployees(){
+        List<EmployeeViewDetailsDTO> employees = employeeService.getAllEmployees();
+        if (!employees.isEmpty()) {
+            return ResponseEntity.ok(employees);
+        } else {
+            return ResponseEntity.status(404).body("No users found");
+        }
+    }
+
+    @GetMapping("/{phone}")
+    public ResponseEntity<?> getEmployeeByPhone(@PathVariable String phone){
+        EmployeeViewDetailsDTO employee = employeeService.getEmployeeByPhone(phone);
+        if (employee != null) {
+            return ResponseEntity.ok(employee);
+        } else {
+            return ResponseEntity.status(404).body("No employee found");
+        }
     }
 
 }
