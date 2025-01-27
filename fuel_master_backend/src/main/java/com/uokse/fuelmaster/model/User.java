a6bcd943ec8e5b7674f1a336a04bbbce380f75b5
thiruniimasha <t.hiruniimasha2002@gmail.com>
2025-01-28 00:31:38 +0530

    fix

--- fuel_master_backend/src/main/java/com/uokse/fuelmaster/model/User.java
+++ fuel_master_backend/src/main/java/com/uokse/fuelmaster/model/User.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 public class User implements UserDetails {
 
     @Id
