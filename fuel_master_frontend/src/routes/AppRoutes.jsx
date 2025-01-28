90132de3ef8e0f0387eb0f0094057aeff7de5e52
Tasheen2002 <darshikatasheen99@gmail.com>
2025-01-28 19:31:38 +0530

    dashboard register page created

--- fuel_master_frontend/src/routes/AppRoutes.jsx
+++ fuel_master_frontend/src/routes/AppRoutes.jsx
@@ -2,8 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import SignUpPage from "@/pages/user/sign_up/sign_up";
 import LoginPage from "@/pages/user/login/login_page";
 import PhoneNumber from "@/pages/user/phone_number/phone_number";
+import VehicleRegistration from "@/pages/user/dashboard/dashboard";
 
-import VerifyOtpPage from '../pages/user/otp/otp_verification';
+import VerifyOtpPage from "../pages/user/otp/otp_verification";
 import AppLayout from "@/layouts/AppLayout";
 import NotFound from "@/pages/error/NotFound";
 
@@ -15,6 +16,7 @@ const AppRoutes = () => {
         <Route path="signup" element={<SignUpPage />} />
         <Route path="otp" element={<VerifyOtpPage />} />
         <Route path="phone" element={<PhoneNumber />} />
+        <Route path="dashboard" element={<VehicleRegistration />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </AppLayout>
