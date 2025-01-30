c6a5882707a9cc1eefcfd067c89e5c775adadc9b
Tasheen2002 <darshikatasheen99@gmail.com>
2025-01-30 12:40:54 +0530

    vehicle type list table created

--- fuel_master_frontend/src/pages/admin/vehicle-types/list-vehicle-types/vehicle-types-table.jsx
+++ fuel_master_frontend/src/pages/admin/vehicle-types/list-vehicle-types/vehicle-types-table.jsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { Edit, Trash2 } from "lucide-react";
 
 const VehicleTypesTable = () => {
-    return (
-        <div className="vehicle-types-table">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Type Name</th>
-                        <th>Description</th>
-                        <th>Capacity</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* Add table rows here */}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Vehicle Type ID</TableHead>
+            <TableHead>Vehicle Type</TableHead>
+            <TableHead>Fuel Type</TableHead>
+            <TableHead>Default Quota</TableHead>
+            <TableHead className="text-right">Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>VT001</TableCell>
+            <TableCell>Sedan</TableCell>
+            <TableCell>Petrol</TableCell>
+            <TableCell>50L</TableCell>
+            <TableCell className="text-right space-x-2">
+              <Button variant="ghost" size="icon">
+                <Edit className="h-4 w-4" />
+              </Button>
+              <Button variant="ghost" size="icon">
+                <Trash2 className="h-4 w-4" />
+              </Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
-export default VehicleTypesTable; 
+export default VehicleTypesTable;
