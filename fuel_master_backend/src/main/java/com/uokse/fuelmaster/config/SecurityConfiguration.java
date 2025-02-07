2fc8856a01735b452a3ef85cd6348f66ca2f481f
HasinthakaPiyumal <hasinthakapiyumal@gmail.com>
2025-02-07 10:21:20 +0530

    Fix typo in CORS allowed origins configuration

--- fuel_master_backend/src/main/java/com/uokse/fuelmaster/config/SecurityConfiguration.java
+++ fuel_master_backend/src/main/java/com/uokse/fuelmaster/config/SecurityConfiguration.java
@@ -77,7 +77,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:5173", "http://localhost:5174",, "http://localhost:5175","https://fuel-master.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:5173", "http://localhost:5174", "http://localhost:5175","https://fuel-master.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration
                 .setAllowedHeaders(List.of("Authorization", "Content-Type", "Origin", "Accept", "X-Requested-With"));
